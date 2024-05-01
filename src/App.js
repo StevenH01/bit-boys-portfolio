f7b7062c772a48e08d66e70275abedcbfe295394
StevenH01 <105683557+StevenH01@users.noreply.github.com>
2024-04-30 23:09:14 -0700

    feat: added pathing and semi functional navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
-import Footer from './component/Footer';
-import HeroSection from './component/HeroSection';
 import NavBar from './component/NavBar';
+import HeroSection from './component/HeroSection';
+import Footer from './component/Footer';
 
+import AlexSection from './component/alex/AlexSection';
+import AngeloSection from './component/angelo/AngeloSection';
+import DannySection from './component/danny/DannySection';
+import HenrySection from './component/henry/HenrySection';
+import JohnSection from './component/john/JohnSection';
+import KevinSection from './component/kevin/KevinSection';
+import SamuelSection from './component/samuel/SamuelSection';
+import StevenSection from './component/steven/StevenSection';
 
 function App() {
   let screen;
@@ -10,16 +18,37 @@ function App() {
     case "/":
       screen = <HeroSection />
       break;
-    // case "":
-    //   screen = ;
-    //   break;
+    case "/alex":
+      screen = <AlexSection />
+      break;
+    case "/angelo":
+      screen = <AngeloSection />;
+      break;
+    case "/danny":
+      screen = <DannySection />;
+      break;
+    case "/henry":
+      screen = <HenrySection />;
+      break;
+    case "/jonathan":
+      screen = <JohnSection />;
+      break;
+    case "/kevin":
+      screen = <KevinSection />;
+      break;
+    case "/samuel":
+      screen = <SamuelSection />;
+      break;
+    case "/steven":
+      screen = <StevenSection />;
+      break;
     default:
       break;
   }
   return (
     <div>
       <NavBar />
-      <HeroSection />
+      {screen}
       <Footer />
     </div>
   );
