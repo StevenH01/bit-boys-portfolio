468252f260a3d6918322c02980b30f5166af50ce
StevenH01 <105683557+StevenH01@users.noreply.github.com>
2024-05-06 23:53:28 -0700

    fix: adjusted logic

--- src/component/steven/SkillSteven.jsx
+++ src/component/steven/SkillSteven.jsx
@@ -23,7 +23,7 @@ const SkillSteven = () => {
 
   return (
     <div
-      className="w-full inline-flex flex-nowrap overflow-hidden"
+      className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]"
       style={{
         WebkitMaskImage: `linear-gradient(to right, transparent 0, black 128px, black calc(100% - 128px), transparent 100%)`,
         maskImage: `linear-gradient(to right, transparent 0, black 128px, black calc(100% - 128px), transparent 100%)`,
