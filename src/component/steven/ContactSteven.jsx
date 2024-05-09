752e12801a46953be537580296444c6c4cc2d608
Steven Ho <steven09ho@gmail.com>
2024-05-08 20:23:13 -0700

    feat: added new sections

---
+++ src/component/steven/ContactSteven.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContactSteven = () => {
+  return <div>ContactSteven</div>;
+};
+
+export default ContactSteven;
---
+++ src/component/steven/ResumeSteven.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ResumeSteven = () => {
+  return <div>ResumeSteven</div>;
+};
+
+export default ResumeSteven;
