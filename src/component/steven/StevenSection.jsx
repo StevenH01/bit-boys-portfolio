0c50b76ddd15cef0bfdfe8ffef2f5bdd81e9e336
Steven Ho <steven09ho@gmail.com>
2024-05-08 20:23:37 -0700

    fix: finished about page and adjusted styling

--- src/component/steven/StevenSection.jsx
+++ src/component/steven/StevenSection.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import SkillSteven from "./SkillSteven";
+import AboutSeven from "./AboutSeven";
+import ExperienceSteven from "./ExperienceSteven";
+import ProjectSteven from "./ProjectSteven";
+import ResumeSteven from "./ResumeSteven";
+import ContactSteven from "./ContactSteven";
 
 const StevenSection = () => {
   return (
-    <div className="p-4">
-      <h1 className="flex justify-center mb-4 underline-offset-4 underline text-2xl text-green-500">
-        Skills
-      </h1>
+    <div className="p-4 scroll-smooth">
       <SkillSteven />
+      <h1 className="flex justify-center mb-4 underline-offset-4 underline text-2xl text-green-500 mt-8">
+        About
+      </h1>
+      <AboutSeven />
+      <h1 className="flex justify-center mb-4 underline-offset-4 underline text-2xl text-green-500">
+        Experience
+      </h1>
+      <ExperienceSteven />
+      <h1 className="flex justify-center mb-4 underline-offset-4 underline text-2xl text-green-500">
+        Projects
+      </h1>
+      <ProjectSteven />
+      <h1 className="flex justify-center mb-4 underline-offset-4 underline text-2xl text-green-500">
+        Resume
+      </h1>
+      <ResumeSteven />
+      <h1 className="flex justify-center mb-4 underline-offset-4 underline text-2xl text-green-500">
+        Contact Me
+      </h1>
+      <ContactSteven />
     </div>
   );
 };
