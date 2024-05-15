0ebefe687bf0a3c9a4596a00735495df03a75bac
StevenH01 <105683557+StevenH01@users.noreply.github.com>
2024-05-14 23:02:50 -0700

    feat: added pdf and download button for resume

--- src/component/steven/ResumeSteven.jsx
+++ src/component/steven/ResumeSteven.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import resume from "../../resumes/steven-resume.pdf";
 
 const ResumeSteven = () => {
-  return <div>ResumeSteven</div>;
+  return (
+    <div className="flex justify-center">
+      <div className="p-6">
+        <a
+          href={resume}
+          className="p-4 rounded-2xl mb-1 bg-gradient-to-br from-green-600 to-teal-400 text-white"
+        >
+          Download CV
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default ResumeSteven;
